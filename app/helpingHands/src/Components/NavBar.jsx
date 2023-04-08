62d79ce25d199796df22ac3350a9eac2bc54265b
Adam Beigel <31280510+abeige@users.noreply.github.com>
2023-04-08 15:07:03 -0500

    missing data

--- app/helpingHands/src/Components/NavBar.jsx
+++ app/helpingHands/src/Components/NavBar.jsx
@@ -10,8 +10,6 @@ const Navbar = (props) => {
       <div className="links">
         <button onClick= {() => props.passedFunc("home")}>Home</button>
         <button onClick= {() => props.passedFunc("workforce_centers")}>Workforce Centers</button>
-        <button onClick= {() => props.passedFunc("food_banks")}>Food Banks</button>
-        <button onClick= {() => props.passedFunc("shelters")}>Shelters</button>
         <button onClick= {() => props.passedFunc("warming_centers")}>Warming Centers</button>
         <button onClick= {() => props.passedFunc("cooling_centers")}>Cooling Centers</button>
         <button onClick= {() => props.passedFunc("community_services")}>Community Services</button>
