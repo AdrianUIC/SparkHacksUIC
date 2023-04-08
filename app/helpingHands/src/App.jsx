4b40251784ceeed26ee1de5d526f1c3e1b6dc648
Christian Sanchez <91746384+ChristianSanchez-gse@users.noreply.github.com>
2023-04-08 00:42:36 -0500

    fixed broken route code

--- app/helpingHands/src/App.jsx
+++ app/helpingHands/src/App.jsx
@@ -1,21 +1,9 @@
 import Navbar from "./NavBar.jsx";
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css'
 
 function App() {
 
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={WorkForceCenters} />
-          <Route path="/about" component={Services} />
-          <Route path="/about" component={Services} />
-          <Route path="/about" component={Services} />
-          <Route path="/about" component={Services} />
-        </Switch>
-      </div>
     <div className="App">
       <div className="navbar-container">
         <Navbar />
@@ -23,7 +11,6 @@ function App() {
       
       <h1>Helping Hands</h1>
     </div>
-    </Router>
   )
 }
 
