d5a4bcd75418c329341dd156e18c57a10de8c65f
Christian Sanchez <91746384+ChristianSanchez-gse@users.noreply.github.com>
2023-04-08 13:25:36 -0500

    Added about page

--- app/helpingHands/src/Components/PageContent.jsx
+++ app/helpingHands/src/Components/PageContent.jsx
@@ -1,10 +1,18 @@
 import ServiceApp from "./ServiceApp"
-
+import '../Styling/PageContent.css'
 function PageContent(props) {
     const page = props.content.toLowerCase()
     if (page == "home") {
         return (
-            <div className='content'>Home</div>
+            <div className='content'>
+                <h1>About</h1>
+                <div>
+                    This is a tool that collects public information from the Chicago Data Portal (https://data.cityofchicago.org/) 
+                    and uses the OpenStreetMaps API in order to populate different types of services that can be useful for
+                    the homeless communities. We aimed to create a site where all of the resources are displayed in a user friendly manner that is both compatible with mobile devices and
+                    desktops. 
+                </div>
+            </div>
         )
     } else {
         return (
