6e6c5b98d08eb8bedb7e0b7ebba8212191da0b54
Christian Sanchez <91746384+ChristianSanchez-gse@users.noreply.github.com>
2023-04-07 23:41:17 -0500

    added navbar (again)

--- app/helpingHands/src/NavBar.jsx
+++ app/helpingHands/src/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import './App.css'
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="#">
-        Navbar
+        Helping Hand Home
       </a>
       <button
         className="navbar-toggler"
@@ -20,50 +21,34 @@ const Navbar = () => {
 
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
         <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">
-              Home <span className="sr-only">(current)</span>
-            </a>
-          </li>
           <li className="nav-item">
             <a className="nav-link" href="#">
-              Features
+              WorkForce Centers
             </a>
           </li>
+
           <li className="nav-item">
             <a className="nav-link" href="#">
               Pricing
             </a>
           </li>
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdownMenuLink"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown link
+
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Pricing
             </a>
-            <div
-              className="dropdown-menu"
-              aria-labelledby="navbarDropdownMenuLink"
-            >
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-              <a className="dropdown-item" href="#">
-                Another action
-              </a>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </div>
           </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Pricing
+            </a>
+          </li>
+          
         </ul>
       </div>
     </nav>
+
   );
 };
 
