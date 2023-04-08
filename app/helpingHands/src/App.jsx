13d5520d15594a3a3fd3ee37be55b717806bf507
Christian Sanchez <91746384+ChristianSanchez-gse@users.noreply.github.com>
2023-04-08 00:39:07 -0500

    created category files

--- app/helpingHands/src/App.jsx
+++ app/helpingHands/src/App.jsx
@@ -1,9 +1,21 @@
 import Navbar from "./NavBar.jsx";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css'
 
 function App() {
 
   return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={WorkForceCenters} />
+          <Route path="/about" component={Services} />
+          <Route path="/about" component={Services} />
+          <Route path="/about" component={Services} />
+          <Route path="/about" component={Services} />
+        </Switch>
+      </div>
     <div className="App">
       <div className="navbar-container">
         <Navbar />
@@ -11,6 +23,7 @@ function App() {
       
       <h1>Helping Hands</h1>
     </div>
+    </Router>
   )
 }
 
