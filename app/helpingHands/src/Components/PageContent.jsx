342f0cee2c337161f7c8bbd3bb56f1ccb4e60d9d
Adam Beigel <31280510+abeige@users.noreply.github.com>
2023-04-10 00:17:46 -0500

    home page

--- app/helpingHands/src/Components/PageContent.jsx
+++ app/helpingHands/src/Components/PageContent.jsx
@@ -7,12 +7,7 @@ function PageContent(props) {
         return (
             <div className='content'>
                 <h1>About</h1>
-                <div>
-                    This is a tool that collects public information from the Chicago Data Portal (https://data.cityofchicago.org/) 
-                    and uses the OpenStreetMaps API in order to populate different types of services that can be useful for
-                    the homeless communities. We aimed to create a site where all of the resources are displayed in a user friendly manner that is both compatible with mobile devices and
-                    desktops. 
-                </div>
+                <p> This is a tool that collects public information from the <a href="https://data.cityofchicago.org/">Chicago Data Portal</a> and uses the OpenStreetMaps API in order to populate different types of services that can be useful for the homeless communities. We aimed to create a site where all of the resources are displayed in a user friendly manner that is both compatible with mobile devices and desktops.</p>
                 <div className="hands"> </div>
             </div>
         )
