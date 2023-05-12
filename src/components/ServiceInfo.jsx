957c0441c99e1e1de2e71603d80e97ad93a80006
Adam Beigel <31280510+abeige@users.noreply.github.com>
2023-05-12 15:18:55 -0500

    got rid of the bootstrap Card

--- src/components/ServiceInfo.jsx
+++ src/components/ServiceInfo.jsx
@@ -1,30 +1,21 @@
 import "leaflet/dist/leaflet.css";
-import { Card } from 'react-bootstrap'
-import { v4 as uuidv4 } from 'uuid';
 
-// TODO: get rid of boostrap
 function ServiceInfo(props) {
   if (props.isLoaded) {
     return (
-      <div className="scroll"> {
+      <div className="overflow-scroll"> {
         props.data.features.map(location => (
-          <Card key={uuidv4()} className="mx-5" style={{ width: '24rem' }}>
-            <Card.Body>
-              <Card.Title>{location.properties.name}</Card.Title>
-              <Card.Text>
-                Address: {location.properties.address}
-                <br />
-                Phone: {location.properties.phone}
-                <br />
-                Hours: {location.properties.hours}
-              </Card.Text>
-            </Card.Body>
-          </Card>
+          <div>
+            <h3>{location.properties.name}</h3>
+            <p> Address: {location.properties.address} </p>
+            <p> Phone: {location.properties.phone} </p>
+            <p> Hours: {location.properties.hours} </p>
+          </div>
         ))
       } </div>
     );
   }
-  return ( <div>Data is loading...</div> );
+  return (<div>Data is loading...</div>);
 }
 
 export default ServiceInfo;
